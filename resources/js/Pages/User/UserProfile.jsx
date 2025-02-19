e3bda60b8fc4fc08a84f9eafc482399a35075ead
Aemonsz2004 <105410652+Aemonsz2004@users.noreply.github.com>
2025-02-19 20:03:27 +0800

    fixed user profile not showing

--- resources/js/Pages/User/UserProfile.jsx
+++ resources/js/Pages/User/UserProfile.jsx
@@ -6,25 +6,28 @@ import React, {useState} from 'react'
 const UserProfile = () => {
 
   const [showForm, setShowForm] = useState(false)
+  const [isSubmitted, setIsSubmitted] = useState(false)
 
 
+  
   return (
     <UserLayout>
 
 <div className='w-full min-h-[100%] bg-white flex items-center justify-center rounded-2xl'>
 
-  {!showForm ? (
-    <button onClick={() => setShowForm(true)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+  {!showForm && !isSubmitted ? (
+    <button onClick={() => {setShowForm(true)}} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
     Register Barangay Profile
     </button>
-  ) : (
+  ) : null}
 
-      <Profile
-    title={'User Profile'}
-    route_name={'pending-resident.store'}
 
+      {showForm && <Profile
+                    title={'User Profile'}
+                    route_name={'pending-resident.store'}
+                    
     />
-  )}
+}
 
 </div>
 
