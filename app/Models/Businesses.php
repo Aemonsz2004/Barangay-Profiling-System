b5be2603ba1ce9e8652396689e9da5df828b2743
Aemonsz2004 <105410652+Aemonsz2004@users.noreply.github.com>
2025-03-05 19:53:56 +0800

    fixed businsesses does not seed

--- app/Models/Businesses.php
+++ app/Models/Businesses.php
@@ -10,17 +10,8 @@ class Businesses extends Model
     use HasFactory;
 
     protected $fillable = [
-        'business_name',
-        'business_address',
-        'business_type',
-        'owner_name',
-        'contact_number',
-        'email',
-        'business_permit_number',
-        'permit_issue_date',
-        'permit_expiry_date',
-        'business_status',
-        'registration_year',
-        'resident_id'
+        'business_name', 'business_address', 'business_type', 'owner_name',
+        'contact_number', 'email', 'business_permit_number', 'permit_issue_date',
+        'permit_expiry_date', 'business_status', 'registration_year', 'resident_id'
     ];
 }
--- database/factories/BusinessesFactory.php
+++ database/factories/BusinessesFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use App\Models\Businesses;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class BusinessFactory extends Factory
+class BusinessesFactory extends Factory
 {
     protected $model = Businesses::class;
 
@@ -17,13 +17,13 @@ class BusinessFactory extends Factory
             'business_type' => $this->faker->randomElement(['Retail', 'Service', 'Manufacturing']),
             'owner_name' => $this->faker->name,
             'contact_number' => $this->faker->phoneNumber,
-            'email' => $this->faker->email,
-            'business_permit_number' => $this->faker->numerify('BP-#####'),
+            'email' => $this->faker->safeEmail,
+            'business_permit_number' => $this->faker->uuid,
             'permit_issue_date' => $this->faker->date,
             'permit_expiry_date' => $this->faker->date,
-            'business_status' => $this->faker->randomElement(['Active', 'Inactive', 'Pending']),
+            'business_status' => $this->faker->randomElement(['Active', 'Inactive']),
             'registration_year' => $this->faker->year,
-            'resident_id' => null,
+            'resident_id' => $this->faker->numberBetween(1, 100),
         ];
     }
 }
